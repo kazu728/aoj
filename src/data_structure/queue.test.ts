d730beb04b86a58ee2d5e877b7f5b16c3a0d1192
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-01-25 10:46:09 +0900

    feat:queue

---
+++ src/data_structure/queue.test.ts
@@ -0,0 +1,9 @@
+import { excute } from "./queue";
+const input = `5 100 
+p1 150
+p2 80
+p3 200
+p4 350
+p5 20`;
+
+excute(input);
---
+++ src/data_structure/queue.ts
@@ -0,0 +1,73 @@
+type JobType = {
+  [key: string]: number;
+};
+
+let head = 0;
+let tail = 0;
+let queue: JobType[];
+let time = 0;
+let remainder;
+
+const enqueue = (a: JobType) => {
+  if (isFull()) throw new Error("overflow");
+  queue[tail] = a;
+  tail++;
+};
+
+const dequeue = (): JobType => {
+  if (isEmpty()) throw new Error("underflow");
+  head++;
+  return queue[head - 1];
+};
+
+const isEmpty = () => {
+  return head === tail;
+};
+
+const isFull = () => {
+  return head === tail + 1;
+};
+
+const convertToObjectArray = (inputArray: string[]): any => {
+  return inputArray
+    .filter(index => index)
+    .map(e => {
+      const [key, value] = e.split(/\s/);
+      return { [key]: value };
+    });
+};
+
+const initialize = (length: number, array: JobType[]) => {
+  queue = new Array(length);
+
+  for (let i = 1; i <= length; i++) {
+    enqueue(array[i]);
+  }
+};
+
+const roundrobin = (quantum: number) => {
+  while (head !== tail) {
+    const job = dequeue();
+    for (let key in job) {
+      const calculateResult = job[key] - quantum;
+
+      if (calculateResult > 0) {
+        enqueue({ [key]: calculateResult });
+        time += quantum;
+      } else {
+        remainder = Math.abs(calculateResult);
+        time += quantum - remainder;
+      }
+      console.log(`${key} ${time}`);
+    }
+  }
+};
+
+export const excute = (input: string) => {
+  const inputArray = input.split("\n");
+  const [length, quantum] = inputArray[0].split(" ").map(e => parseInt(e));
+  const array = convertToObjectArray(inputArray);
+
+  initialize(length, array);
+  roundrobin(quantum);
+};
