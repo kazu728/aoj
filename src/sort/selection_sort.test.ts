527c57cb51433c92116d3480b09a641fb65ece5d
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-01-26 12:41:20 +0900

    feat:selection_sort

---
+++ src/sort/selection_sort.test.ts
@@ -0,0 +1,25 @@
+import {
+  generateInputValue,
+  generateInputValueBigArray,
+  parseInputValue,
+  sortByApi
+} from "../utility";
+import { sort } from "./selection_sort";
+
+test("should sort", () => {
+  const MEMORY_LIMIT = 65536000;
+  const input = generateInputValue();
+  const array = parseInputValue(input);
+
+  expect(sortByApi(array)).toEqual(sort(input));
+  expect(process.memoryUsage().heapUsed).toBeLessThan(MEMORY_LIMIT);
+});
+
+test("should sort big array", () => {
+  const MEMORY_LIMIT = 65536000;
+  const input = generateInputValueBigArray();
+  const array = parseInputValue(input);
+
+  expect(sortByApi(array)).toEqual(sort(input));
+  expect(process.memoryUsage().heapUsed).toBeLessThan(MEMORY_LIMIT);
+});
---
+++ src/sort/selection_sort.ts
@@ -0,0 +1,20 @@
+import { generateInputArray } from "../utility";
+
+let swap;
+
+export const sort = (input: string) => {
+  const [length, array] = generateInputArray(input);
+  
+  for (let i = 0; i < length; i++) {
+    let min = array[i];
+    for (let j = i; j < length; j++) {
+      if (array[j] < min) {
+        swap = min;
+        min = array[j];
+        array[j] = swap;
+      }
+    }
+    array[i] = min;
+  }
+  return array;
+};
