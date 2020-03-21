58b224c4f9e32e4b35cc8ba57f2eea95f711bc34
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-03-21 17:23:50 +0900

    chore:add web-db-press-115

---
+++ src/chore/web-db-press115/submission.test.ts
@@ -0,0 +1,10 @@
+import { dfs } from "./submission";
+
+const N = 4;
+const K = 3;
+
+test("should ", () => {
+  console.log(dfs(N, K, 1));
+
+  expect(dfs(N, K, 1)).toEqual(10);
+});
---
+++ src/chore/web-db-press115/submission.ts
@@ -0,0 +1,10 @@
+// A: 整数を２倍にする
+// B: Kを足した値にする
+
+// A or BをN回行う
+
+export const dfs = (remain: number, K: number, current: number): any => {
+  if (remain === 0) return current;
+  
+  return Math.min(dfs(remain - 1, K, current + K), dfs(remain - 1, K, 2 * current));
+};
