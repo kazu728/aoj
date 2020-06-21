48737f4704a25d45773f2c7b0a3d79463bd7b455
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-06-21 10:04:28 +0900

    chore:removed node

--- src/mathmatics/fibonacci_sequence.test.ts
+++
@@ -1,5 +0,0 @@
-import main from './fibonacci_sequence'
-
-test('fibonacci', () => {
-  expect(main()[3]).toEqual(3)
-})
--- src/tree/node/binary_search_tree.ts
+++
@@ -1,8 +0,0 @@
-export class BinarySearchTreeNode {
-  constructor(
-    public key: number,
-    public left: BinarySearchTreeNode,
-    public right: BinarySearchTreeNode,
-    public parent: BinarySearchTreeNode
-  ) {}
-}
