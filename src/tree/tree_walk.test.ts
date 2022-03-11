79d9fd926d2770e934c8e4a5e2ac68fed3337a1d
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-03-11 12:48:15 -0800

    refactor: tree walk

--- src/tree/tree_walk.test.ts
+++ src/tree/tree_walk.test.ts
@@ -1,25 +1,32 @@
 import { assertEquals } from "../deps.ts";
-import init, { inorder, postOrder, preorder } from "./tree_walk.ts";
+import { genNodeTree, Node } from "./utility.ts";
+import { inorder, postorder, preorder } from "./tree_walk.ts";
+type InputType = [number, ...[...number[]][]];
 
-Deno.test("tree walk", () => {
-  type InputType<T> = [number, ...[...T[]][]];
+const input: InputType = [
+  9,
+  [0, 1, 4],
+  [1, 2, 3],
+  [2, -1, -1],
+  [3, -1, -1],
+  [4, 5, 8],
+  [5, 6, 7],
+  [6, -1, -1],
+  [7, -1, -1],
+  [8, -1, -1],
+];
+const omitInput = input.filter((e, i): e is number[] => i !== 0);
+const [node, left, right] = omitInput[0];
+const treeNode = genNodeTree(omitInput, node, -1, left, right) as Node;
 
-  const input: InputType<number> = [
-    9,
-    [0, 1, 4],
-    [1, 2, 3],
-    [2, -1, -1],
-    [3, -1, -1],
-    [4, 5, 8],
-    [5, 6, 7],
-    [6, -1, -1],
-    [7, -1, -1],
-    [8, -1, -1],
-  ];
+Deno.test("preorder", () => {
+  assertEquals(preorder(treeNode, []), [0, 1, 2, 3, 4, 5, 6, 7, 8]);
+});
 
-  init(input);
+Deno.test("inorer", () => {
+  assertEquals(inorder(treeNode, []), [2, 1, 3, 0, 6, 5, 7, 4, 8]);
+});
 
-  assertEquals(preorder(0), [0, 1, 2, 3, 4, 5, 6, 7, 8]);
-  assertEquals(inorder(0), [2, 1, 3, 0, 6, 5, 7, 4, 8]);
-  assertEquals(postOrder(0), [2, 3, 1, 6, 7, 5, 8, 4, 0]);
+Deno.test("postorder", () => {
+  assertEquals(postorder(treeNode, []), [2, 3, 1, 6, 7, 5, 8, 4, 0]);
 });
