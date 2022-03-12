5a399bc1d17b42219b24f97cc311dbb01ee4f2aa
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-03-12 15:42:20 -0800

    feat: exhaustive search

--- src/recursive/exhaustive_search.test.ts
+++ src/recursive/exhaustive_search.test.ts
@@ -1,11 +1,11 @@
 import { assertEquals } from "../deps.ts";
-import { InputType } from "./exhaustive_search.ts";
-import search from "./exhaustive_search.ts";
+import { main } from "./exhaustive_search.ts";
 
 Deno.test("Exhaustive search", () => {
-  const input: InputType = [
-    [5, [1, 5, 7, 10, 21]],
-    [4, [2, 4, 17, 8]],
-  ];
-  assertEquals(search(input), [17, 8]);
+  const input = [
+    [1, 5, 7, 10, 21],
+    [2, 4, 17, 8],
+  ] as [number[], number[]];
+
+  assertEquals(main(...input), [false, false, true, true]);
 });
