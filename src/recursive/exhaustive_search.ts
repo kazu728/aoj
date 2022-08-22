f8d41c177fd544810e88ed6df8c0f02876f9fab6
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-08-22 09:15:36 +0900

    feat: exhaustive search

--- src/recursive/exhaustive_search.ts
+++ src/recursive/exhaustive_search.ts
@@ -1,17 +1,10 @@
-const go = (a: number[], m: number, i: number): boolean => {
-  if (m === 0) return true;
+export function main(a: number[], b: number[]): boolean[] {
+  return b.map((n) => canDivide(n, a, 0));
+}
+
+const canDivide = (n: number, a: number[], i: number): boolean => {
+  if (n === 0) return true;
   if (a.length < i) return false;
 
-  return go(a, m, i + 1) || go(a, m - a[i], i + 1);
+  return canDivide(n, a, i + 1) || canDivide(n - a[i], a, i + 1);
 };
-
-export function main(a: number[], b: number[]): boolean[] {
-  const l: boolean[] = [];
-
-  for (const m of b) {
-    const canCreate = go(a, m, 0);
-    l.push(canCreate);
-  }
-
-  return l;
-}
