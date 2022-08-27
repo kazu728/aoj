2b20ad92af2d9b877d56e4ed9dc78c28e4c07145
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2022-08-27 22:20:24 +0900

    feat: binary tree

--- src/tree/binary_tree.ts
+++ src/tree/binary_tree.ts
@@ -1,72 +1,82 @@
 import { KIND } from "./tree.d.ts";
-import { genNodeTree, Node, MaybeNode } from "./utility.ts";
+import { genNodeTree, MaybeNode, Node } from "./utility.ts";
 
 type Output = {
   node: number;
   parent: number;
+  sibling: number;
   degree: number;
+  depth: number;
   height: number;
   kind: KIND;
 };
 
-export type BinaryTreeInput = [number, ...[...number[]][]];
+export type BinaryTreeInput = number[][];
 export type BinaryTreeOutput = Output[];
-type OmitTreeInput = number[][];
 
-const getHeight = (node: Node, height: number): number => {
-  const lheight = node.l === undefined ? height : getHeight(node.l, height + 1);
-  const rHeight = node.r === undefined ? height : getHeight(node.r, height + 1);
+export function main(input: BinaryTreeInput): BinaryTreeOutput {
+  const rootNode = genTree(input, 0);
+  return genOutput(rootNode, []);
+}
 
-  return Math.max(lheight, rHeight);
+const genTree = (input: BinaryTreeInput, index: number): Node => {
+  const rootNode = genNodeTree(
+    input,
+    index,
+    undefined,
+    input[0][1],
+    input[0][2]
+  );
+  if (rootNode === undefined) throw new Error("Unexpected");
+
+  return rootNode;
+};
+
+const genOutput = (node: Node, acc: Output[]): Output[] => {
+  acc.push({
+    node: node.value!,
+    degree: getDegree(node),
+    depth: getDepth(node, 0),
+    height: getheight(node, 0),
+    parent: node.parent?.value === undefined ? -1 : node.parent?.value,
+    sibling: getSibling(node) || -1,
+    kind: getKind(node),
+  });
+
+  if (node.l !== undefined) genOutput(node.l, acc);
+  if (node.r !== undefined) genOutput(node.r, acc);
+
+  return acc;
 };
 
 const getDegree = (node: Node): number => {
   if (node.l !== undefined && node.r !== undefined) return 2;
-  if (node.l === undefined && node.r !== undefined) return 1;
-  if (node.l !== undefined && node.r === undefined) return 1;
-  return 0;
+  if (node.l === undefined && node.r === undefined) return 0;
+
+  return 1;
 };
 
 const getKind = (node: Node): KIND => {
-  if (node.parent?.value === -1) return "root";
-  if (node.l !== undefined || node.r !== undefined) return "internal node";
-  return "leaf";
+  if (node.parent === undefined) return "root";
+  if (node.l === undefined && node.r === undefined) return "leaf";
+
+  return "internal node";
 };
 
-const traverse = (node: Node, output: BinaryTreeOutput): BinaryTreeOutput => {
-  const height = getHeight(node, 0);
-  const degree = getDegree(node);
-  const kind = getKind(node);
-
-  const o: Output = {
-    node: node.value!,
-    parent: node.parent?.value!,
-    height,
-    degree,
-    kind,
-  };
-  output.push(o);
-
-  if (node.l !== undefined) traverse(node.l, output);
-  if (node.r !== undefined) return traverse(node.r, output);
-
-  return output;
+const getSibling = (node: Node): number | undefined => {
+  if (node.value === node.parent?.r?.value) return node.parent?.l?.value;
+  if (node.value === node.parent?.l?.value) return node.parent?.r?.value;
 };
 
-export function main(input: BinaryTreeInput): BinaryTreeOutput {
-  const omitInput: OmitTreeInput = input.filter(
-    (_, i): _ is number[] => i !== 0
-  );
-  const [node, leftNode, rightNode] = omitInput[0];
+const getDepth = (node: Node, depth: number): number => {
+  if (node.parent === undefined) return depth;
+  return getDepth(node.parent, depth + 1);
+};
 
-  const nodeTree: MaybeNode = genNodeTree(
-    omitInput,
-    node,
-    -1,
-    leftNode,
-    rightNode
-  );
-  if (nodeTree === undefined) return [];
+const getheight = (node: MaybeNode, height: number): number => {
+  if (node?.r === undefined && node?.l === undefined) return height;
+  const left = getheight(node.l, height + 1) || -1;
+  const right = getheight(node.r, height + 1) || -1;
 
-  return traverse(nodeTree, []);
-}
+  return Math.max(left, right);
+};
