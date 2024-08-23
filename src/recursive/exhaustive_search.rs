ff127bf10e322e00eaae49a1c988b87f33093460
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2024-08-23 21:51:23 +0900

    exhaustive serarch dp

--- src/recursive/exhaustive_search.rs
+++ src/recursive/exhaustive_search.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn can_compose_a_from_b<'a>(a: &[i32], b: &[i32], result: &'a mut [bool]) -> &'a [bool] {
     for (i, e) in a.iter().enumerate() {
         result[i] = can_divide(*e, b, 0);
@@ -16,6 +18,21 @@ fn can_divide(a: i32, b: &[i32], i: usize) -> bool {
     can_divide(a, b, i + 1) || can_divide(a - b[i], b, i + 1)
 }
 
+pub fn can_compose_a_from_b_dp<'a>(a: &[i32], b: &[i32], result: &'a mut [bool]) -> &'a [bool] {
+    let mut dp: HashSet<i32> = HashSet::new();
+
+    for &a in b.iter() {
+        dp.extend(dp.iter().map(|&x| x + a).collect::<Vec<i32>>());
+        dp.insert(a);
+    }
+
+    for (i, a) in a.iter().enumerate() {
+        result[i] = dp.get(a).is_some();
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,6 +44,17 @@ mod tests {
 
         assert_eq!(
             can_compose_a_from_b(a, b, &mut [false; 4]),
+            &[false, false, true, true]
+        )
+    }
+
+    #[test]
+    fn test_can_compose_a_from_b_dp() {
+        let a = &[2, 4, 17, 8];
+        let b = &[1, 5, 7, 10, 21];
+
+        assert_eq!(
+            can_compose_a_from_b_dp(a, b, &mut [false; 4]),
             &[false, false, true, true]
         )
     }
